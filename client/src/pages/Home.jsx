e27f9913783983c1aa61325df40f49144695cb4a
Karthick Kumar SM <sivakumarkarthickkumar@gmail.com>
2024-01-31 23:39:37 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React,{useState,useEffect} from 'react'
-import {Loader,card,FormField} from '../components'
+import {Loader,Card,FormField} from '../components'
 
 const RenderCards=({data,title})=>{
     if(data?.length>0){
-    return data.map((post)=><card key={post._id}{...post}/>);
+    return data.map((post)=><Card key={post._id}{...post}/>);
   }
   return(
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
